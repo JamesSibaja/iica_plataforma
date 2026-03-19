7e6712fe07df18c5c1ae8738973905f228ec98aa
JamesSibaja <jsibajagranados2@gmail.com>
2026-03-19 17:23:10 -0600

    setup.sh arreglo

--- iica_plataforma/website_management/models.py
+++ iica_plataforma/website_management/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.db.utils import ProgrammingError
 
 class UserProfile(models.Model):
 
@@ -25,8 +26,12 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        try:
+            UserProfile.objects.create(user=instance)
+        except ProgrammingError:
+            pass
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+
